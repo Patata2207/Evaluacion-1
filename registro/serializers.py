abd69ae91ffaf8596d6cc88c0e7c6264e520597d
Patata2207 <matias.herrera62@inacapmail.cl>
2025-11-22 13:52:58 -0300

    wow

--- registro/serializers.py
+++ registro/serializers.py
@@ -4,16 +4,28 @@ from .models import Visita
 
 
 class VisitaSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Visita
         fields = [
             "url",
+            "id",
             "nombre",
             "rut",
             "motivo",
             "hora_entrada",
-            "hora_salida"
+            "hora_salida",
         ]
+
+    def validate(self, attrs):
+        hora_entrada = attrs.get("hora_entrada") or getattr(self.instance, "hora_entrada", None)
+        hora_salida = attrs.get("hora_salida") or getattr(self.instance, "hora_salida", None)
+        if hora_entrada and hora_salida and hora_salida < hora_entrada:
+            raise serializers.ValidationError({
+                "hora_salida": "La hora de salida debe ser mayor o igual a la hora de entrada."
+            })
+        return attrs
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
